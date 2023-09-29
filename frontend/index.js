3c414578ad45f344f1730e2c5e2bb17a1eeaabba
Mitch Hammond <mitch.b.hammond@gmail.com>
2023-09-29 11:23:43 -0600

    got the first two tasks down.

--- frontend/index.js
+++ frontend/index.js
@@ -37,6 +37,12 @@ function moduleProject2() {
       row.appendChild(square)
       square.addEventListener('click', () => {
         // 👉 TASK 2 - Use a click handler to target a square 👈
+        if(!square.classList.contains('targeted')){
+           getAllSquares().forEach( square => {
+           document.querySelector('.targeted').classList.remove('targeted')
+           square.classList.add('targeted')
+           })
+        }
       })
     }
   }
